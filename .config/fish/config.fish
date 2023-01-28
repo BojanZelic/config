83d02310c53b32d28acf2351e103f0955b7461d4
Bojan Zelic <bojan.zelic@qualia.com>
2023-01-27 17:46:50 -0700

    slow fish startup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,3 @@
-
 export GOBIN=$HOME/go/bin
 
 set -gx PATH {$GOBIN} /opt/homebrew/bin /opt/homebrew/sbin {$GOPATH}/bin {$GOROOT}/bin ~/.local/bin /snap/bin $PATH
@@ -6,17 +5,15 @@ set -gx PATH $PATH $HOME/.krew/bin
 set -gx PATH $PATH $HOME/.linkerd2/bin
 
 export GOPATH=$HOME/go
-export GOROOT=(brew --prefix go@1.19)/libexec
-export GPG_TTY=(tty)
+#export GOROOT=(brew --prefix go@1.19)/libexec
+#export GPG_TTY=(tty)
 
 # Path to Oh My Fish install.
 set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
-
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
-
 #source completions/vault.fish
 source ~/.config/fish/config.variables.fish
 set -xg SUMMON_PROVIDER "/bin/cat"
@@ -31,4 +28,4 @@ alias mk='minikube --alsologtostderr'
 alias homeconfig '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 #kubectl completion fish | source
 
-status --is-interactive; and rbenv init - fish | source
+#status --is-interactive; and rbenv init - fish | source
